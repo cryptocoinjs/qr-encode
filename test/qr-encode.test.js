f01078272a5b861600ce11ccf445c77e03b8238d
JP Richardson <jprichardson@gmail.com>
2014-07-02 18:23:02 -0500

    tests passing

--- test/qr-encode.test.js
+++ test/qr-encode.test.js
@@ -1,2 +1,22 @@
+var assert = require('assert')
 
+var qr = require('../')
 
+var fixtures = require('./fixtures/qr-encode')
+
+describe('qr-encode', function() {
+  fixtures.valid.forEach(function(f) {
+    it('should encode ' + f.text + ' size: ' + f.size + ' level: ' + f.level + ' type: ' + f.type, function() {
+      var data = qr(f.text, {type: f.type, size: f.size, level: f.level})
+      assert.equal(data, f.src)
+    })
+  })
+
+  fixtures.invalid.forEach(function(f) {
+    it('should throw ' + f.text + ' size: ' + f.size + ' level: ' + f.level + ' type: ' + f.type, function() {
+      assert.throws(function() {
+        var data = qr(f.text, {type: f.type, size: f.size, level: f.level})
+      },f.error)
+    })
+  })
+})
